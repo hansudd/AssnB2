ede68a7713b485474005df679fce039e21851662
hansudd <hanweisudd@gmail.com>
2021-11-16 18:51:04 -0800

    editing roxygen tags

--- R/datasnapshot.R
+++ R/datasnapshot.R
@@ -1,4 +1,31 @@
-
+#' @title
+#' Data Snapshot for Two Numeric Variables
+#'
+#' @description
+#' Function provides snapshot of the linear relationship between two numeric variables, as well as some
+#' summary statistics for numeric variables.
+#'
+#' @usage data_snapshot(data, x, y)
+#'
+#' @details
+#' When given two numeric variables, x and y, this function produces summary statistics and a plot
+#' that allows the user to get a quick snapshot of the data and in particular their linear relationship
+#' (if any).
+#'
+#' @params data needs to be a data frame (for e.g., a tibble)
+#' @params x This input is a numeric variable from a tibble or dataframe, for e.g. column with numeric values
+#' @params y This input is a numeric variable from a tibble or dataframe, for e.g. column with numeric values
+#'
+#' @return If both x and y inputs are numeric and data is a data frame or tibble,
+#'            then the function returns a list object.
+#'            The list contains 4 objects:
+#'            - summary table for y numeric variable (max, min, mean, median)
+#'            - summary table for x numeric variable (max, min, mean, median)
+#'            - scatter plot for x and y
+#'            - adjusted r squared coefficient to indicate linear relationship of x vs y variables
+#' @examples
+#' data_snapshot(mtcars, hp, mpg)
+#'
 
 data_snapshot <- function(data, x, y) {
 
