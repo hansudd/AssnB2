5451d817eb543d9d6d932cac1804f5dc65f4e14a
hansudd <hanweisudd@gmail.com>
2021-11-18 11:06:37 -0800

    minor edits to royxgen tags

--- R/datasnapshot.R
+++ R/datasnapshot.R
@@ -22,12 +22,13 @@
 #' data_snapshot(mtcars, hp, mpg)
 #' data_snapshot(gapminder, lifeExp, gdpPercap)
 #'
+#' @export
+#'
 #' @importFrom dplyr summarise
 #' @importFrom ggplot2 ggplot aes geom_point labs theme_bw
 #' @importFrom broom glance
 #' @importFrom gapminder gapminder
 #'
-#' @export
 
 data_snapshot <- function(data, x, y) {
 
